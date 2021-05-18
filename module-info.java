8cb3aca4f77027dfae21e0a7d76b3b36e2c955e7
Froilyn Poblacion <froilynp13@gmai.com>
2021-05-18 16:25:07 +0800

    Bank

--- module-info.java
+++ module-info.java
@@ -1,2 +1,13 @@
-module Endterm_Exam {
+import javax.swing.JOptionPane;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		Checking Account = new Checking(1);
+		Savings  Savings = new Savings(0, 0);
+		
+		JOptionPane.showMessageDialog(null, Account.getAccountInfo());
+		JOptionPane.showMessageDialog(null, Savings.getAccountInfo());
+	}
 }
